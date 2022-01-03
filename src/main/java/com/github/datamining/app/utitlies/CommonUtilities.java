918921f6bb313c2c1cea33e47c78bda7fd58fbe8
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-01-04 00:18:25 +0100

    Refactor file names and indent size

---
+++ src/main/java/com/github/datamining/app/utitlies/CommonUtilities.java
@@ -0,0 +1,103 @@
+package com.github.datamining.app.utitlies;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/*
+    this class is aimed to contain useful methods that we will need
+    in the gui.
+
+ */
+public class CommonUtilities {
+
+    public  static  void acceptOnlyNumbersTextField(TextField txt){
+      /*
+         just accept Numbers in a specific textField
+       */
+        txt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    txt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+    }
+
+    public String[] return_centrale(ArrayList<Double[]> data, int column){
+        String[] centrale= new String[5];
+        centrale[0]=String.format("%.2f", app.functions.ViewUtilities.getMoy(data,column)).replace(",", ".") ;
+        centrale[1]=String.format("%.2f", app.functions.ViewUtilities.moyenneTranquee(data,column)).replace(",", ".");
+        centrale[2]= app.functions.ViewUtilities.modeFct(data,column).toString();
+        centrale[3]=String.format("%.2f", app.functions.ViewUtilities.getMediane(data,column)).replace(",", ".");
+        //TODO; try to fix the .2f error
+        centrale[4]=String.valueOf(app.functions.ViewUtilities.milieuEtendu(data,column));
+
+        return centrale;
+    }
+
+    public String[] return_dispersion(ArrayList<Double[]> data, int column){
+        String[] dispersion= new String[8];
+        dispersion[0]=String.format("%.2f", app.functions.ViewUtilities.quartiles(data,1,column)).replace(",", ".") ;
+        dispersion[1]=String.format("%.2f", app.functions.ViewUtilities.quartiles(data,2,column)).replace(",", ".") ;
+        dispersion[2]=String.format("%.2f", app.functions.ViewUtilities.quartiles(data,3,column)).replace(",", ".") ;
+        dispersion[3]=String.format("%.2f", app.functions.ViewUtilities.ecartType(data,column)).replace(",", ".");
+        dispersion[4]=String.format("%.2f", app.functions.ViewUtilities.ecartInterquartile(data,column)).replace(",", ".");
+        dispersion[5]=String.format("%.2f", app.functions.ViewUtilities.etendu(data,column)).replace(",", ".");
+        dispersion[6]=String.format("%.2f", app.functions.ViewUtilities.variance(data,column)).replace(",", ".");
+        dispersion[7]= app.functions.ViewUtilities.outliers(data,column).toString();
+
+        return dispersion;
+    }
+
+    public static void addChoice(ComboBox cbx){
+       /*
+            when a user selects an item in the choice box and wants to go back
+            we need to add a "" item that's what this function do
+        */
+
+        cbx.getItems().addAll("area","perimeter","compactness","length of kernel",
+                "width of kernel","assymmetry coeff","length of kernel groove");
+        /*
+        cbx_softwareNeeds.getItems().add("");
+        cbx_desktopEnvirnment.getItems().add("");
+        cbx_linuxDistro.getItems().add("");
+        cbx_os.getItems().add("");
+        */
+
+    }
+
+    public static Boolean isFilled(TextField input){
+        //works !
+        return  !(input.getText() == null || input.getText().trim().isEmpty()) ;
+    }
+
+    public void switchWindow(ActionEvent event, String fxmlWindow, Parent root, Stage stage, Scene scene) throws IOException {
+
+        String cssPath = "/resources/views/style.css";
+        String css = this.getClass().getResource(cssPath).toExternalForm();
+
+        root = FXMLLoader.load(getClass().getResource(fxmlWindow));
+        stage =(Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        scene.getStylesheets().add(css);
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+}
